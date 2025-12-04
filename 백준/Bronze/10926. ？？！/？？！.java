dd15c01486211b4c82b1050d29f5d18e9b0aef38
LimTheo <in05013300@gmail.com>
2025-12-05 00:45:55 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String str = input.nextLine();
+
+        System.out.println(str + "??!");
+    }
+}
